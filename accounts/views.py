02f8ded69095682df390eb5da47e3076906b7455
Manish <sharmagarg007@gmail.com>
2021-03-25 18:41:55 +0530

    completed register and login functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth import authenticate
 from .models import User, Role
-
+from teachers.models import TeacherProfiles
 
 class SignUpView(View):
 	context = dict()
 	def get(self, request, *args, **kwargs):
-		return render(request, 'signup.html', self.context)
+		return render(request, 'register.html', self.context)
 
 	def post(self, request, *args, **kwargs):
 		role = request.POST.get("role")
@@ -29,11 +29,32 @@ class SignUpView(View):
 				user_obj.set_password(password)
 				user_obj.role_id = role_obj
 				user_obj.save()
+
+				# save teacher profile
+				teacher_obj = TeacherProfiles()
+				teacher_obj.first_name = first_name
+				teacher_obj.last_name = last_name
+				teacher_obj.user = user_obj
+				teacher_obj.save()
 			elif role == "school":
 				user_obj = User()
+				user_obj.first_name = first_name
+				user_obj.last_name = last_name
+				user_obj.email = email
+				user_obj.phone = phone
+				user_obj.set_password(password)
+				user_obj.role_id = role_obj
+				user_obj.save()
+
+				# save school profile
+				school_profile_obj = SchoolProfile()
+				school_profile_obj.school_name = user_obj.first_name
+				school_profile_obj.school_email = user_obj.email
+				school_profile_obj.user = user_obj
+				school_profile_obj.save()
 			return redirect('login_view')
 		except Exception as exp:
-			return render(request, 'signup.html', self.context)
+			return render(request, 'register.html', self.context)
 
 
 
@@ -41,8 +62,12 @@ class LoginView(View):
 	context = dict()
 	def get(self, request, *args, **kwargs):
 		return render(request, "login.html", self.context)
+
 	def post(self, request, *args, **kwargs):
 		email = request.POST.get("email")
 		password = request.POST.get("password")
-		user = authenticate(request, email=email, password=password)
-		return render(request, 'signup.html', self.context)
+		try:
+			user = authenticate(request, email=email, password=password)
+			return render(request, 'dashboard.html', self.context)
+		except Exception as exp:
+			return render(request, "login.html", self.context)
---
+++ static/assets/js/custom.js
@@ -0,0 +1,14 @@
+//$(document).ready(function(){ 
+//    $('.toggleMenu1').click(function() { 
+//          $('.menuList1').toggleClass('show');
+//		  $('.menuList2').removeClass('show');
+//	});
+//	$('.toggleMenu2').click(function() {
+//          $('.menuList2').toggleClass('show');
+//		  $('.menuList1').removeClass('show');
+//	});
+//});
+
+
+ 
+ 
